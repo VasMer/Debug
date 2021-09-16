4d2923455157d6e2a5249af547d515aa2c250d68
Vassili Merzin <vas.mer@gmail.com>
2021-09-16 21:02:20 +0300

    Added millis at start debug message

--- include/DebugUtil.h
+++ include/DebugUtil.h
@@ -1,9 +1,11 @@
 #ifdef DEBUG
-#define DEB(x)                       \
+#define DEB(x)                         \
+    Serial.print(millis());            \
+    Serial.print(" ");                 \
     Serial.print(__PRETTY_FUNCTION__); \
     Serial.print("[");                 \
     Serial.print(__LINE__);            \
-    Serial.print("]: ");                 \
+    Serial.print("]: ");               \
     Serial.println(x);
 
 #else
